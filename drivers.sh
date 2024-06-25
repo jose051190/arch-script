a0e96f1175d21a37595d9a34630bbbccadfd2e81
jose051190 <98242282+jose051190@users.noreply.github.com>
2024-06-25 08:13:00 -0300

    Atualizar o drivers.sh

--- drivers.sh
+++ drivers.sh
@@ -7,3 +7,18 @@ check_command() {
     exit 1
   fi
 }
+
+# Habilitar multilib e outras configurações do pacman
+sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf
+check_command "sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf"
+
+# Descomentar configurações no pacman.conf e adicionar ILoveCandy
+sed -i 's/^#Color/Color/' /etc/pacman.conf
+check_command "sed -i 's/^#Color/Color/' /etc/pacman.conf"
+sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
+check_command "sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf"
+sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
+check_command "sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf"
+grep -q '^ParallelDownloads = 5' /etc/pacman.conf && sed -i '/^ParallelDownloads = 5/a ILoveCandy' /etc/pacman.conf || echo -e '\nParallelDownloads = 5\nILoveCandy' >> /etc/pacman.conf
+check_command "adicionar ILoveCandy"
+

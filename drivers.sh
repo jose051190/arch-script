f3cec9279bfe9855f0efc89199f89c37c4b542ba
jose051190 <98242282+jose051190@users.noreply.github.com>
2024-06-25 13:25:58 -0300

    Atualizar o drivers.sh

--- drivers.sh
+++ drivers.sh
@@ -19,10 +19,17 @@ sudo sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 check_command "sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf"
 sudo sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 check_command "sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf"
-grep -q '^ParallelDownloads = 5' /etc/pacman.conf && sudo sed -i '/^ParallelDownloads = 5/a ILoveCandy' /etc/pacman.conf || echo -e '\nParallelDownloads = 5\nILoveCandy' >> /etc/pacman.conf
-check_command "adicionar ILoveCandy"
+
+if grep -q '^ParallelDownloads = 5' /etc/pacman.conf; then
+  sudo sed -i '/^ParallelDownloads = 5/a ILoveCandy' /etc/pacman.conf
+  check_command "adicionar ILoveCandy"
+else
+  echo -e '\nParallelDownloads = 5\nILoveCandy' | sudo tee -a /etc/pacman.conf
+  check_command "adicionar ParallelDownloads e ILoveCandy"
+fi
+
 sudo pacman -Syu
-check_command "sudo pacman -Syy"
+check_command "sudo pacman -Syu"
 
 # Adicionar parâmetros da NVIDIA ao GRUB
 sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& nvidia-drm.modeset=1/' /etc/default/grub
@@ -32,6 +39,10 @@ check_command "grub-mkconfig -o /boot/grub/grub.cfg"
 
 # Instalar yay
 cd /tmp/
+if [ -d "yay" ]; then
+  rm -rf yay
+  check_command "remover diretório yay existente"
+fi
 git clone https://aur.archlinux.org/yay.git
 check_command "git clone https://aur.archlinux.org/yay.git"
 cd yay
@@ -40,13 +51,14 @@ check_command "makepkg -si"
 
 # Pacotes adicionais do AUR
 yay -S --needed --noconfirm onlyoffice-bin visual-studio-code-bin pdfsam protonup-qt nvidia-beta-dkms nvidia-utils-beta lib32-nvidia-utils-beta opencl-nvidia-beta lib32-opencl-nvidia-beta nvidia-settings bottles dxvk-bin proton-ge-custom protontricks protonup-qt wine-installer gamescope-nvidia
+check_command "yay -S pacotes adicionais do AUR"
 
 # Configurar drivers da NVIDIA
 sudo pacman -S --needed --noconfirm libva-nvidia-driver vulkan-icd-loader lib32-vulkan-icd-loader egl-wayland libvdpau-va-gl libvdpau
 check_command "pacman -S pacotes NVIDIA"
 
 # Criar arquivo de configuração do modprobe para NVIDIA
-sudo cat <<EOL > /etc/modprobe.d/nvidia.conf
+sudo tee /etc/modprobe.d/nvidia.conf > /dev/null <<EOL
 options nvidia_drm modeset=1
 EOL
 check_command "criação de /etc/modprobe.d/nvidia.conf"
@@ -65,6 +77,7 @@ check_command "systemctl enable nvidia-hibernate.service"
 sudo systemctl enable nvidia-resume.service
 check_command "systemctl enable nvidia-resume.service"
 
+# Instalar pacotes essenciais
 sudo pacman -S --needed --noconfirm efibootmgr dialog os-prober ntfs-3g mtools dosfstools linux-headers bluez bluez-utils bluez-plugins git xdg-utils wget curl
 check_command "pacman -S pacotes essenciais"
 
@@ -76,4 +89,3 @@ check_command "systemctl start bluetooth.service"
 
 # Finalizar instalação
 exit
-

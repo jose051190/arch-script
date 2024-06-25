1b868b3a23d5b3cb7114f8cab35835c5a862575d
jose051190 <98242282+jose051190@users.noreply.github.com>
2024-06-25 13:21:56 -0300

    Atualizar o drivers.sh

--- drivers.sh
+++ drivers.sh
@@ -9,25 +9,25 @@ check_command() {
 }
 
 # Habilitar multilib e outras configurações do pacman
-sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf
+sudo sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf
 check_command "sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf"
 
 # Descomentar configurações no pacman.conf e adicionar ILoveCandy
-sed -i 's/^#Color/Color/' /etc/pacman.conf
+sudo sed -i 's/^#Color/Color/' /etc/pacman.conf
 check_command "sed -i 's/^#Color/Color/' /etc/pacman.conf"
-sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
+sudo sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf
 check_command "sed -i 's/^#VerbosePkgLists/VerbosePkgLists/' /etc/pacman.conf"
-sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
+sudo sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf
 check_command "sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 5/' /etc/pacman.conf"
-grep -q '^ParallelDownloads = 5' /etc/pacman.conf && sed -i '/^ParallelDownloads = 5/a ILoveCandy' /etc/pacman.conf || echo -e '\nParallelDownloads = 5\nILoveCandy' >> /etc/pacman.conf
+grep -q '^ParallelDownloads = 5' /etc/pacman.conf && sudo sed -i '/^ParallelDownloads = 5/a ILoveCandy' /etc/pacman.conf || echo -e '\nParallelDownloads = 5\nILoveCandy' >> /etc/pacman.conf
 check_command "adicionar ILoveCandy"
-pacman -Syu
-check_command "pacman -Syy"
+sudo pacman -Syu
+check_command "sudo pacman -Syy"
 
 # Adicionar parâmetros da NVIDIA ao GRUB
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& nvidia-drm.modeset=1/' /etc/default/grub
+sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& nvidia-drm.modeset=1/' /etc/default/grub
 check_command "sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"[^\"]*/& nvidia-drm.modeset=1/' /etc/default/grub"
-grub-mkconfig -o /boot/grub/grub.cfg
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 check_command "grub-mkconfig -o /boot/grub/grub.cfg"
 
 # Instalar yay
@@ -39,39 +39,39 @@ makepkg -si --noconfirm
 check_command "makepkg -si"
 
 # Pacotes adicionais do AUR
-#yay -S --needed --noconfirm onlyoffice-bin visual-studio-code-bin pdfsam protonup-qt nvidia-beta-dkms nvidia-utils-beta lib32-nvidia-utils-beta opencl-nvidia-beta lib32-opencl-nvidia-beta nvidia-settings bottles dxvk-bin proton-ge-custom protontricks protonup-qt wine-installer gamescope-nvidia
+yay -S --needed --noconfirm onlyoffice-bin visual-studio-code-bin pdfsam protonup-qt nvidia-beta-dkms nvidia-utils-beta lib32-nvidia-utils-beta opencl-nvidia-beta lib32-opencl-nvidia-beta nvidia-settings bottles dxvk-bin proton-ge-custom protontricks protonup-qt wine-installer gamescope-nvidia
 
 # Configurar drivers da NVIDIA
-pacman -S --needed --noconfirm libva-nvidia-driver vulkan-icd-loader lib32-vulkan-icd-loader egl-wayland libvdpau-va-gl libvdpau
+sudo pacman -S --needed --noconfirm libva-nvidia-driver vulkan-icd-loader lib32-vulkan-icd-loader egl-wayland libvdpau-va-gl libvdpau
 check_command "pacman -S pacotes NVIDIA"
 
 # Criar arquivo de configuração do modprobe para NVIDIA
-cat <<EOL > /etc/modprobe.d/nvidia.conf
+sudo cat <<EOL > /etc/modprobe.d/nvidia.conf
 options nvidia_drm modeset=1
 EOL
 check_command "criação de /etc/modprobe.d/nvidia.conf"
 
 # Atualizar o initramfs
-sed -i 's/^MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+sudo sed -i 's/^MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
 check_command "sed -i 's/^MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf"
-mkinitcpio -P
+sudo mkinitcpio -P
 check_command "mkinitcpio -P"
 
 # Habilitar serviços NVIDIA
-systemctl enable nvidia-suspend.service
+sudo systemctl enable nvidia-suspend.service
 check_command "systemctl enable nvidia-suspend.service"
-systemctl enable nvidia-hibernate.service
+sudo systemctl enable nvidia-hibernate.service
 check_command "systemctl enable nvidia-hibernate.service"
-systemctl enable nvidia-resume.service
+sudo systemctl enable nvidia-resume.service
 check_command "systemctl enable nvidia-resume.service"
 
-pacman -S --needed --noconfirm efibootmgr dialog os-prober ntfs-3g mtools dosfstools linux-headers bluez bluez-utils bluez-plugins git xdg-utils wget curl
+sudo pacman -S --needed --noconfirm efibootmgr dialog os-prober ntfs-3g mtools dosfstools linux-headers bluez bluez-utils bluez-plugins git xdg-utils wget curl
 check_command "pacman -S pacotes essenciais"
 
 # Habilitar serviços
-systemctl enable bluetooth.service
+sudo systemctl enable bluetooth.service
 check_command "systemctl enable bluetooth.service"
-systemctl start bluetooth.service
+sudo systemctl start bluetooth.service
 check_command "systemctl start bluetooth.service"
 
 # Finalizar instalação

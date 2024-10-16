cd8547f363063245a3aa4cd149cb72a165712d4a
jose051190 <98242282+jose051190@users.noreply.github.com>
2024-10-16 17:51:02 -0300

    Atualizar o arch_install_amd_part3.sh

--- arch_install_amd_part3.sh
+++ arch_install_amd_part3.sh
@@ -8,9 +8,32 @@ check_command() {
   fi
 }
 
-# Instalar Plasma e pacotes adicionais sem confirmação
-sudo pacman -S --needed plasma-meta plasma-workspace konsole okular sddm xorg-xwayland filelight ffmpeg ffmpegthumbs ffmpegthumbnailer nextcloud-client ttf-nerd-fonts-symbols inter-font ttf-jetbrains-mono elisa gwenview plymouth kwayland kwayland-integration kfind xwaylandvideobridge konsole kwrite packagekit-qt6 ark egl-wayland dolphin dolphin-plugins xdg-desktop-portal-kde okular spectacle partitionmanager qt6-multimedia qt6-multimedia-gstreamer qt6-multimedia-ffmpeg qt6-wayland kdeplasma-addons kalk sweeper krename plasma-systemmonitor kdeconnect kio-gdrive lokalize kde-dev-utils kompare ghostwriter kclock neovim firefox-i18n-pt-br plasma-firewall ttf-fira-sans ttf-roboto-mono-nerd ttf-fira-mono rclone telegram-desktop fastfetch htop ncdu virt-manager qemu-full ebtables iptables-nft dnsmasq edk2-ovmf spice-vdagent virt-viewer pycharm-community-edition firewalld
-check_command "pacman -S plasma-desktop e outros pacotes"
+# Perguntar ao usuário se deseja instalar Plasma ou GNOME
+echo "Qual ambiente de desktop deseja instalar? (Digite 1 para Plasma ou 2 para GNOME)"
+read desktop_choice
+
+if [ "$desktop_choice" == "1" ]; then
+  # Instalar Plasma e pacotes adicionais sem confirmação
+  sudo pacman -S --needed plasma-meta plasma-workspace konsole okular sddm xorg-xwayland filelight ffmpeg ffmpegthumbs ffmpegthumbnailer nextcloud-client ttf-nerd-fonts-symbols inter-font ttf-jetbrains-mono elisa gwenview plymouth kwayland kwayland-integration kfind xwaylandvideobridge konsole kwrite packagekit-qt6 ark egl-wayland dolphin dolphin-plugins xdg-desktop-portal-kde okular spectacle partitionmanager qt6-multimedia qt6-multimedia-gstreamer qt6-multimedia-ffmpeg qt6-wayland kdeplasma-addons kalk sweeper krename plasma-systemmonitor kdeconnect kio-gdrive lokalize kde-dev-utils kompare ghostwriter kclock neovim firefox-i18n-pt-br plasma-firewall ttf-fira-sans ttf-roboto-mono-nerd ttf-fira-mono rclone telegram-desktop fastfetch htop ncdu virt-manager qemu-full ebtables iptables-nft dnsmasq edk2-ovmf spice-vdagent virt-viewer pycharm-community-edition firewalld
+  check_command "pacman -S plasma-desktop e outros pacotes"
+
+  # Habilitar SDDM
+  sudo systemctl enable sddm.service
+  check_command "sudo systemctl enable sddm.service"
+  
+elif [ "$desktop_choice" == "2" ]; then
+  # Instalar GNOME e pacotes adicionais sem confirmação
+  sudo pacman -S --needed gnome xorg-xwayland ffmpeg ffmpegthumbs ffmpegthumbnailer nextcloud-client ttf-nerd-fonts-symbols inter-font ttf-jetbrains-mono plymouth xdg-desktop-portal-gnome neovim firefox-i18n-pt-br ttf-fira-sans ttf-roboto-mono-nerd ttf-fira-mono rclone telegram-desktop fastfetch htop ncdu virt-manager qemu-full ebtables iptables-nft dnsmasq edk2-ovmf spice-vdagent virt-viewer pycharm-community-edition firewalld
+  check_command "pacman -S gnome e outros pacotes"
+
+  # Habilitar GDM
+  sudo systemctl enable gdm.service
+  check_command "sudo systemctl enable gdm.service"
+  
+else
+  echo "Opção inválida. Por favor, escolha 1 para Plasma ou 2 para GNOME."
+  exit 1
+fi
 
 # Configurar GRUB para Plymouth
 sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/&splash rd.udev.log_priority=3 vt.global_cursor_default=0 /' /etc/default/grub
@@ -21,8 +44,8 @@ sudo grub-mkconfig -o /boot/grub/grub.cfg
 check_command "sudo grub-mkconfig -o /boot/grub/grub.cfg"
 
 # Adicionar plymouth ao vetor HOOKS em mkinitcpio.conf após base e udev
-sudo sed -i '/^HOOKS=/ s/\(base udev\)/\1 plymouth/' /etc/mkinitcpio.conf
-check_command "sudo sed -i '/^HOOKS=/ s/\(base udev\)/\1 plymouth/' /etc/mkinitcpio.conf"
+sudo sed -i '/^HOOKS=/ s/base udev/\1 plymouth/' /etc/mkinitcpio.conf
+check_command "sudo sed -i '/^HOOKS=/ s/base udev/\1 plymouth/' /etc/mkinitcpio.conf"
 
 # Atualizar mkinitcpio
 sudo mkinitcpio -p linux
@@ -30,10 +53,7 @@ check_command "sudo mkinitcpio -p linux"
 
 # Ativação e início do serviço libvirtd
 sudo systemctl enable --now libvirtd.service
-
-# Habilitar SDDM
-sudo systemctl enable sddm.service
-check_command "sudo systemctl enable sddm.service"
+check_command "sudo systemctl enable --now libvirtd.service"
 
 # Habilitar Firewall
 sudo systemctl enable firewalld.service
@@ -48,7 +68,7 @@ makepkg -si --noconfirm
 check_command "makepkg -si"
 
 # Perguntar ao usuário se deseja reiniciar
-echo "Instalação do KDE Plasma e pacotes adicionais concluída. Deseja reiniciar o sistema agora? (s/n)"
+echo "Instalação concluída. Deseja reiniciar o sistema agora? (s/n)"
 read resposta
 
 if [ "$resposta" == "s" ]; then

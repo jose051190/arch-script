44a529566023495e59ea8548b568ce85ead0c7c1
jose051190 <98242282+jose051190@users.noreply.github.com>
2025-05-26 21:03:58 -0300

    Update arch_install_parte1.sh

--- arch_install_parte1.sh
+++ arch_install_parte1.sh
@@ -12,7 +12,7 @@ BTRFS_OPTS="defaults,noatime,compress=zstd"
 PACOTES_BASE="base linux linux-firmware nano base-devel intel-ucode networkmanager network-manager-applet bash-completion linux-headers"
 
 # Scripts que serão copiados para o chroot
-SCRIPTS=("arch_part2.sh" "arch_part3.sh")
+SCRIPTS=("arch_install_parte2.sh" "arch_install_parte3.sh")
 
 # Log
 LOG_FILE="install.log"
